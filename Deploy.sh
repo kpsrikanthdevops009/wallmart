b8ba2389c148c446cdeed75e872cf1a5747a46e9
srikanthdevops <kp.srikanth009@gmail.com>
2023-05-14 17:33:07 +0530

    updated

--- Deploy.sh
+++ Deploy.sh
@@ -1,2 +1,8 @@
+<<<<<<< HEAD
 Hello This File Contains deployement scripot
 iam in stagfe branch s
+=======
+Hello This File Contains deployement scripots
+
+iu am a master branch
+>>>>>>> 54f2397 (updated)
